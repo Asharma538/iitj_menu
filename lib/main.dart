1537b417fe38a0619939da78aa5114d76efcea07
Anadi Sharma <sharma.130@iitj.ac.in>
2023-07-26 18:56:52 +0530

    Timeline designed and Menu for the day fetched

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:menu_app/firebase_options.dart';
@@ -20,13 +20,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.orange),
       debugShowCheckedModeBanner: false,
-      home: const Home(),
+      home: Home(),
     );
   }
 }
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
+  static Map<int,String> weekday_name = {
+    0:"Sunday",
+    1:"Monday",
+    2:"Tuesday",
+    3:"Wednesday",
+    4:"Thursday",
+    5:"Friday",
+    6:"Saturday",
+  };
+  static List<dynamic> Breakfast=[];
+  static List<dynamic> Lunch=[];
+  static List<dynamic> Snacks=[];
+  static List<dynamic> Dinner=[];
 
   @override
   State<Home> createState() => _HomeState();
@@ -34,8 +47,21 @@ class Home extends StatefulWidget {
 
 
 class _HomeState extends State<Home> {
+  fetchMenu() async{
+    var instance = FirebaseFirestore.instance;
+    var day_today = Home.weekday_name[DateTime.now().weekday];
+    final docRef = instance.collection("Menu").doc(day_today);
+    await docRef.get().then((DocumentSnapshot snapshot)  {
+      final data = snapshot.data() as Map<String,dynamic>;
+      Home.Breakfast = data["Breakfast"]!;
+      Home.Lunch = data["Lunch"]!;
+      Home.Snacks = data["Snacks"]!;
+      Home.Dinner = data["Dinner"]!;
+    });
+  }
   @override
   Widget build(BuildContext context) {
+    fetchMenu();
     return Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
@@ -45,7 +71,7 @@ class _HomeState extends State<Home> {
               padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
               child: IconButton(
                 onPressed: () {
-                  
+
                 },
                 icon: Icon(Icons.restaurant_menu),
               ),
@@ -54,13 +80,13 @@ class _HomeState extends State<Home> {
         ),
         body: Center(
           child: Container(
-            margin: EdgeInsets.fromLTRB(25, 20, 25, 0),
+            margin: EdgeInsets.fromLTRB(15, 20, 25, 0),
               child: Column(
                 children: [
-                  Meal(context,"Breakfast", "Pav Bhaji","", "7:00 AM", "10:00 AM"),
-                  Meal(context,"Lunch", "Pav Bhaji","", "12:15 PM", "2:30 PM"),
-                  Meal(context,"Snacks", "Pav Bhaji","", "5:30 PM", "6:30 PM"),
-                  Meal(context,"Dinner", "Pav Bhaji","", "8:00 PM", "10:00 PM"),
+                  Meal(context,"Breakfast", Home.Breakfast[0],Home.Breakfast[1], "7:00 AM", "10:00 AM",TimeOfDay(hour: 7, minute: 0),TimeOfDay(hour: 10, minute: 0)),
+                  Meal(context,"Lunch", Home.Lunch[0],Home.Lunch[1], "12:15 PM", "2:30 PM",TimeOfDay(hour: 12, minute: 15),TimeOfDay(hour: 14, minute: 30)),
+                  Meal(context,"Snacks", Home.Snacks[0],Home.Snacks[1], "5:30 PM", "6:30 PM",TimeOfDay(hour: 17, minute: 30),TimeOfDay(hour: 19, minute: 0)),
+                  Meal(context,"Dinner", Home.Dinner[0],Home.Dinner[1], "8:00 PM", "10:00 PM",TimeOfDay(hour: 20, minute: 0),TimeOfDay(hour: 22, minute: 0)),
                 ],
               ),
           ),
@@ -70,15 +96,29 @@ class _HomeState extends State<Home> {
 }
 
 Widget Meal(
-    BuildContext context, String Name, String Items,String SideItems, String start, String end) {
+  BuildContext context, String Name, String Items,String SideItems, String start, String end,TimeOfDay meal_start_time,TimeOfDay meal_end_time) {
+  var start_dt = new DateTime(DateTime.now().year,DateTime.now().month,DateTime.now().day,meal_start_time.hour,meal_start_time.minute);
+  var end_dt = new DateTime(DateTime.now().year,DateTime.now().month,DateTime.now().day,meal_end_time.hour,meal_end_time.minute);
+
+  Color? active = Colors.orange[600];
+
+  if (DateTime.now().isAfter(start_dt) && DateTime.now().isBefore(end_dt)){
+    active = Colors.teal;
+  }
+  print(active);
+
   return TimelineTile(
+    beforeLineStyle: LineStyle(
+      color: active!,
+    ),
+    afterLineStyle: LineStyle(
+      color: active,
+    ),
     alignment: TimelineAlign.start,
     indicatorStyle: IndicatorStyle(
-      width: 2,
+      color: active,
+      width: 20,
       indicatorXY: 0.5,
-      iconStyle: IconStyle(
-        iconData: Icons.circle,fontSize: 20,
-      )
     ),
     endChild: Container(
       constraints: BoxConstraints(
